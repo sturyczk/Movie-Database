b191ea7c9690941c03995ef1749fe3670903d05c
sturyczk <kylie.sturycz@gmail.com>
2024-10-17 18:30:01 -0500

    Started the movies class
    I add the movies constructor and the getTitle and getGenre methods

--- MovieDatabase/src/Movies.java
+++ MovieDatabase/src/Movies.java
@@ -1,4 +1,17 @@
 
 public class Movies {
-
+	private String title;
+	private String genre;
+	
+	public Movies(String title, String genre) {
+		this.title=title;
+		this.genre=genre;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public String getGenre() {
+		return genre;
+	}
 }
